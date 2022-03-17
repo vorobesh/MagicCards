0ae24ff04a1d8868caea0b86527aa2c0ee450e31
Nikita <mnextn@mail.ru>
2022-03-17 21:56:19 +0300

    profileBack

--- db/models/user.js
+++ db/models/user.js
@@ -9,8 +9,8 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
-      // define association here
+    static associate({ City }) {
+      User.belongsTo(City, { foreignKey: 'id_city' });
     }
   }
   User.init({
